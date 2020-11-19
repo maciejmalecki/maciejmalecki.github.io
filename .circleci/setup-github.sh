d3d5466749c579e2028d99e49284a703044c0321
Maciej Malecki <maciej.malecki@capgemini.com>
2020-11-19 23:48:54 +0100

    fix deployment

--- .circleci/setup-github.sh
+++ .circleci/setup-github.sh
@@ -3,8 +3,8 @@
 git config user.name "$USER_NAME"
 git config user.email "$USER_EMAIL"
 
-git checkout master
-git pull origin master
+git checkout gh-pages
+git pull origin gh-pages
 
 find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
 mv _site/* .
@@ -12,6 +12,6 @@ rm -R _site/
 
 git add -fA
 git commit --allow-empty -m "$(git log master -1 --pretty=%B)"
-git push origin master:gh-pages
+git push -f origin gh-pages
 
 echo "deployed successfully"
