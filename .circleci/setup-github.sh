e17b40daa09344782331a5d2bf7f89ae67c799f3
Maciej Malecki <maciej.malecki@capgemini.com>
2020-11-20 20:43:29 +0100

    yet

--- .circleci/setup-github.sh
+++ .circleci/setup-github.sh
@@ -3,15 +3,19 @@
 git config user.name "$USER_NAME"
 git config user.email "$USER_EMAIL"
 
+ls -al
+
 git stash
 git checkout gh-pages
 git pull origin gh-pages
+
+ls -al
 
 find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name '.circleci' -exec rm -rf {} \;
 mv _site/* .
 rm -R _site/
 
-ls -alR
+ls -al
 
 git add -fA
 git commit --allow-empty -m "$(git log master -1 --pretty=%B)"
